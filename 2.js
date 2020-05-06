2c435a5a31a6543f1a02c16309de49184079e8e5
zjlyyq <2868989685@qq.com>
2020-05-06 09:22:23 +0800

    w2

--- 2.js
+++ 2.js
@@ -3,7 +3,7 @@ var ff = 1;
 first_function(ff);
 // console.log(first_var);
 function first_function(first_var){
-    //  var first_var = 1;
+     var first_var = 1;
      second_function();  
 }
 function second_function(){
